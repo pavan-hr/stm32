41b55d088dd5d8b08068b0674a81161164eb9c65
hr-pavan <pavan.hr@arable.com>
2025-03-09 18:03:05 +0530

    task creation

---
+++ 001Task/Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/portable.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/task.h \
+ /home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/list.h
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/portable.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/task.h:
+/home/pavanhr/STM32CubeIDE/workspace_1.18.0/course_FreeRTOS/RTOS_Workspace/stm32/001Task/ThirdParty/FreeRTOS/include/list.h:
