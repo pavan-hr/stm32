08c48e832446347d4d2acf3e3668e1bf47e2a4f9
hr-pavan <pavan.hr@arable.com>
2025-03-10 20:14:20 +0530

    segger printf over Uart implemented. logs are seen in sysView application

--- 001Task/Core/Src/main.c
+++ 001Task/Core/Src/main.c
@@ -100,9 +100,11 @@ int main(void)
   //Enable the CYCCNT counter.
   DWT_CTRL |= (1 << 0);
 
+  SEGGER_UART_init(500000);
+
   //call the below API to get the logs in sysview app
   SEGGER_SYSVIEW_Conf();
-  SEGGER_SYSVIEW_Start();
+  //SEGGER_SYSVIEW_Start(); //will be called in segger_uart.c file
 
 
   status = xTaskCreate(task1_handler, "Task_1", 200,"Hello World from Task 1", 2, &task1_handle);
@@ -154,8 +156,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -171,7 +173,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
